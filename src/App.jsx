bf7b49a20138f6ad22e6ba304e96af0eb6a8412f
Alan García <alangarcianavarro@hotmail.com>
2024-06-26 16:45:28 +0200

    Instalar React Materiaul UI. Carga de imágenes. Diseño imagen principal

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>
-
+      <div className="the-pillows-background">
+        <div id="presentation" className="the-pillows-background-text">
+          <p>OH YEAH?</p>
+          <p>Do you know me!?</p>
+          <p>Our name is <strong>
+            <a href="https://en.wikipedia.org/wiki/The_Pillows" target="_blank">the pillows</a></strong>, OK?
+          </p>
+        </div>
       </div>
     </>
   )
