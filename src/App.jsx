2a78d6310dcf1049626827e05b8b3e352ef5b90a
Alan García <alangarcianavarro@hotmail.com>
2024-06-27 19:46:49 +0200

    Apply container sections

--- src/App.jsx
+++ src/App.jsx
@@ -82,101 +82,105 @@ function App() {
         </div>
       </div>
 
-      <div className='container'>
-        <h1 className='title'>"Oh Yeah" examples compilation</h1>
+      <div className='container-full'>
+        <div className='container'>
+          <h1 className='title'>"Oh Yeah" examples compilation</h1>
 
-        <div id="compilation">
-          <p>In order to the context of these charts, some examples of <strong>"oh yeah"</strong> catch-phrase compilation that vocalist Sawao Yamanaka says in many songs, although present charts <strong><ins>actually only counting "yeah", and not "oh yeah"</ins></strong>.</p>
+          <div id="compilation">
+            <p>In order to the context of these charts, some examples of <strong>"oh yeah"</strong> catch-phrase compilation that vocalist Sawao Yamanaka says in many songs, although present charts <strong><ins>actually only counting "yeah", and not "oh yeah"</ins></strong>.</p>
 
-          <p>The video's original author below is <a href="https://www.youtube.com/user/sononamida/" target="_blank">sononamida</a>, so watch it directly from YouTube as appreciation.</p>
+            <p>The video's original author below is <a href="https://www.youtube.com/user/sononamida/" target="_blank">sononamida</a>, so watch it directly from YouTube as appreciation.</p>
 
-          <div id="compilation-video">
-            <iframe width="640" height="480" src="https://www.youtube.com/embed/Y08_zl7Qhdg" allowFullScreen style={{ border: 'none' }}></iframe>
+            <div id="compilation-video">
+              <iframe width="640" height="480" src="https://www.youtube.com/embed/Y08_zl7Qhdg" allowFullScreen style={{ border: 'none' }}></iframe>
 
-            <iframe width="640" height="480" src="https://www.youtube.com/embed/z3mqVlymwVA" allowFullScreen style={{ border: 'none' }}></iframe>
+              <iframe width="640" height="480" src="https://www.youtube.com/embed/z3mqVlymwVA" allowFullScreen style={{ border: 'none' }}></iframe>
+            </div>
           </div>
         </div>
 
-        <div id="ranking">
-          <h1 className='title'>Yeah Ranking Songs</h1>
+        <div className='container'>
+          <div id="ranking">
+            <h1 className='title'>Yeah Ranking Songs</h1>
 
-          <div className='ranking-yeahs'>
-            <div>
-              <div className="yeah-total">
-                <h3>Total current "yeahs"</h3>
+            <div className='ranking-yeahs'>
+              <div>
+                <div className="yeah-total">
+                  <h3>Total current "yeahs"</h3>
+                  <div>
+                    <span className="yeah-podium-numbers">647</span>
+                  </div>
+                </div>
+              </div>
+              
+              <div className='ranking-top-3'>
                 <div>
-                  <span className="yeah-podium-numbers">647</span>
+                  <h3>1<sup>st</sup> song</h3>
+                  <p className="yeah-podium-numbers yeah-first-position">76</p>
+                  <p className="song-winner-size">Like a Lovesong (Back to Back)</p>
+                  <p className="text-center album">Album: Little Busters (1998)</p>
+                </div>
+
+                <div>
+                  <h3>2<sup>nd</sup> song</h3>
+                  <p className="yeah-podium-numbers yeah-second-position">42</p>
+                  <p className="song-winner-size">Rookie Jet</p>
+                  <p className="text-center album">Album: Thank you, My Twilight (2002)</p>
+                </div>
+
+                <div>
+                  <h3>3<sup>rd</sup> song</h3>
+                  <p className="yeah-podium-numbers yeah-third-position">35</p>
+                  <p className="song-winner-size">Back Seat Dog</p>
+                  <p className="text-center album">Album: Happy Bivouac (1999)</p>
+                </div>
+              </div>
+
+              <div className='ranking-first-last-yeah-song'>
+                <div>
+                  <h3 className="text-center mb-3 font-weight-bold">1<sup>st</sup> "yeah" song</h3>
+                  <div className="text-center mb-3">
+                    <p className="song-first-yeah song-winner-size">Never Find</p>
+                  </div>
+                  <p className="text-center album">Album: 90's My Life (1990)</p>
+                </div>
+
+                <div>
+                  <h3 className="text-center mb-3 font-weight-bold">Last "yeah" song</h3>
+                  <div className="text-center mb-3">
+                    <p className="song-last-yeah song-winner-size">Before going bed</p>
+                  </div>
+                  <p className="text-center album">Album: Rebroadcast (2018)</p>
                 </div>
               </div>
             </div>
-            
-            <div className='ranking-top-3'>
-              <div>
-                <h3>1<sup>st</sup> song</h3>
-                <p className="yeah-podium-numbers yeah-first-position">76</p>
-                <p className="song-winner-size">Like a Lovesong (Back to Back)</p>
-                <p className="text-center album">Album: Little Busters (1998)</p>
-              </div>
 
-              <div>
-                <h3>2<sup>nd</sup> song</h3>
-                <p className="yeah-podium-numbers yeah-second-position">42</p>
-                <p className="song-winner-size">Rookie Jet</p>
-                <p className="text-center album">Album: Thank you, My Twilight (2002)</p>
-              </div>
+            <div id="yeah-overall">
+              <h1 className="title">Yeah Overall</h1>
 
-              <div>
-                <h3>3<sup>rd</sup> song</h3>
-                <p className="yeah-podium-numbers yeah-third-position">35</p>
-                <p className="song-winner-size">Back Seat Dog</p>
-                <p className="text-center album">Album: Happy Bivouac (1999)</p>
-              </div>
-            </div>
-
-            <div className='ranking-first-last-yeah-song'>
-              <div>
-                <h3 className="text-center mb-3 font-weight-bold">1<sup>st</sup> "yeah" song</h3>
-                <div className="text-center mb-3">
-                  <p className="song-first-yeah song-winner-size">Never Find</p>
+              <div className='yeah-overall-container'>
+                <div className='yeah-overall-row'>
+                  <p>Total years with some "yeahs"! <span className="yeah-overall-emoji-size">😀</span>
+                  </p>
+                  <ProgressWithLabel value={24} size={35} color={`#28a745`} />
                 </div>
-                <p className="text-center album">Album: 90's My Life (1990)</p>
-              </div>
 
-              <div>
-                <h3 className="text-center mb-3 font-weight-bold">Last "yeah" song</h3>
-                <div className="text-center mb-3">
-                  <p className="song-last-yeah song-winner-size">Before going bed</p>
+                <div className='yeah-overall-row'>
+                  <p>Total years with "yeahs" in a row! <span className="yeah-overall-emoji-size">😄</span>
+                  </p>
+                  <ProgressWithLabel value={16} size={35} color={`#28a745`} />
                 </div>
-                <p className="text-center album">Album: Rebroadcast (2018)</p>
+                
+                <div className='yeah-overall-row'>
+                  <p>Total years with no "yeahs" <span className="yeah-overall-emoji-size">☹️</span>
+                  </p>
+                  <ProgressWithLabel value={10} size={35} color={`#dc3545`} />
+                </div>
               </div>
             </div>
           </div>
         </div>
-
-        <div id="yeah-overall">
-          <h1 className="title">Yeah Overall</h1>
-
-          <div className='yeah-overall-container'>
-            <div className='yeah-overall-row'>
-              <p>Total years with some "yeahs"! <span className="yeah-overall-emoji-size">😀</span>
-              </p>
-              <ProgressWithLabel value={24} size={35} color={`#28a745`} />
-            </div>
-
-            <div className='yeah-overall-row'>
-              <p>Total years with "yeahs" in a row! <span className="yeah-overall-emoji-size">😄</span>
-              </p>
-              <ProgressWithLabel value={16} size={35} color={`#28a745`} />
-            </div>
-            
-            <div className='yeah-overall-row'>
-              <p>Total years with no "yeahs" <span className="yeah-overall-emoji-size">☹️</span>
-              </p>
-              <ProgressWithLabel value={10} size={35} color={`#dc3545`} />
-            </div>
-          </div>
-        </div>
-
+        
         <div id="yeahs-per-year-container">
           <h3 className="title">Number of "yeahs" per year</h3>
 
